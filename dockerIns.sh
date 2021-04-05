00b8a11fb9bfe5e13ae2cf153de6805804f38522
LeeHanSeong7 <54903040+LeeHanSeong7@users.noreply.github.com>
2021-04-06 08:30:56 +0900

    Update dockerIns.sh

--- dockerIns.sh
+++ dockerIns.sh
@@ -6,11 +6,9 @@ sudo apt-get install -y \
     curl \
     gnupg \
     lsb-release
-Y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-Y
