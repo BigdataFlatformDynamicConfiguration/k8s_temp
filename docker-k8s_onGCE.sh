4e4b349988a8c3144115ad556040a0689bcc4fba
caresin <whdqja2155@naver.com>
2021-04-10 04:45:20 +0900

    Update docker-k8s_onGCE.sh

--- docker-k8s_onGCE.sh
+++ docker-k8s_onGCE.sh
@@ -19,4 +19,5 @@ echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https:/
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
-
+#matsernode_init : sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=EC2_인스턴스_내부아이피
+#workenode_join : sudo kubeadm join 마스터노드_프라이빗_아이피:6443  --token 토큰  --discovery-token-ca-cert-hash sha256:디스커버리_토큰
